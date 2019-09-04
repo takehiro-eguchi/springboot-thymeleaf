178670e9bc6f47193681e8b10a356cc431fb2a5f
Takehiro Eguchi <t-eguchi@nssys.co.jp>
2019-09-04 17:05:12 +0900

    初期化

---
+++ src/main/java/com/egu/springthymeleaf/Sample.java
@@ -0,0 +1,5 @@
+package com.egu.springthymeleaf;
+
+public class Sample {
+
+}
---
+++ src/test/java/com/egu/springthymeleaf/SampleTest.java
@@ -0,0 +1,14 @@
+package com.egu.springthymeleaf;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
